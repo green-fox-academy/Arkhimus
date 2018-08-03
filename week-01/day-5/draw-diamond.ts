e864ca3364dd65d14c37c3a4b4269130f978725a
Adam Reznek <reznek.adam@gmail.com>
2018-08-03 13:11:36 +0200

    add(file): diamond

--- week-01/day-5/draw-diamond.ts
+++ week-01/day-5/draw-diamond.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 7;
+let lineCount: number = 13;
 
 // Write a program that draws a
 // diamond like this:
@@ -17,12 +17,27 @@ let lineCount: number = 7;
 
 let a: string = ' ';
 let b: string = '*';
+let c: string = '*';
+let d: string = '  ';
 
-for (let i: number = 0; i < lineCount; i++) {
-    for (let j: number = i; j < (lineCount - 1); j++) {
+
+for (let i: number = 0; i < lineCount/2; i++) {
+    for (let j: number = i; j < (lineCount / 2 - 1 ); j++) {
         a += ' ';
     }
     console.log( a + b );
+    if (lineCount % 2 == 0 && i == lineCount/2 - 1) {
+        console.log( a + b );
+    }
     b += '**';
     a = ' ';
+}
+
+for (let v: number = 2; v < (lineCount/2 + 1); v++) {
+    for (let g: number = v; g < lineCount/2; g++) {
+        c += '**';
+    }
+    console.log( d + c );
+    d += ' ';
+    c = '*';
 }
--- week-01/day-5/test.ts
+++ week-01/day-5/test.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-let lineCount: number = 7;
+let lineCount: number = 8;
 
 // Write a program that draws a
 // diamond like this:
@@ -19,15 +19,32 @@ let lineCount: number = 7;
 //UPSIDE DOWN TRIANGLE
 
 
+let a: string = ' ';
+let b: string = '*';
+
+
+
+for (let i: number = 0; i < lineCount/2; i++) {
+    for (let j: number = i; j < lineCount/2 - 1; j++) {
+        a += ' ';
+    }
+    console.log( a + b );
+
+    if (i == lineCount/2 - 1 && lineCount % 2 == 0) {
+        console.log(a + b)
+    }
+    b += '**';
+    a = ' ';
+}
+
 let c: string = '*';
 let d: string = '';
 
-for (let v: number = 0; v < lineCount; v++) {
-    for (let g: number = v; g < (lineCount - 1); g++) {
+for (let v: number = 0; v < lineCount/2 + 1; v++) {
+    for (let g: number = v; g < (lineCount/2); g++) {
         c += '**';
     }
     console.log( d + c );
     d += ' ';
     c = '*';
 }
-
