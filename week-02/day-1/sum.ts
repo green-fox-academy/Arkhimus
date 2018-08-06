3b2ddccda4e2f84c7c4bcc4d6e5105dcf5458835
Adam Reznek <reznek.adam@gmail.com>
2018-08-06 15:07:37 +0200

    add(file): sum

---
+++ week-02/day-1/sum.ts
@@ -0,0 +1,16 @@
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
+
+export{}
+
+let mustSum: number = 6;
+
+function sum (num: number) {
+    let j: number = 0;
+    for (let i:number = 0; i <= num; i++) {
+        j = j += i;
+    };
+    return j;
+}
+
+console.log(sum(mustSum));
