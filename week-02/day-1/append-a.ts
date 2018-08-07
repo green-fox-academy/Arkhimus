a8a9bc1bd8c75cf3c6f0c89fa48c2eb58263c859
Adam Reznek <reznek.adam@gmail.com>
2018-08-07 09:50:31 +0200

    add(file): simple replace.ts

---
+++ week-02/day-1/append-a.ts
@@ -0,0 +1,17 @@
+export{}
+
+// -  Create an array variable named `animals`
+//    with the following content: `["koal", "pand", "zebr"]`
+// -  Add all elements an `"a"` at the end
+
+let animals: string [] = ["koal", "pand", "zebr"];
+
+let appendAnimals
+
+/*
+function appendA(text: string) {
+    text = typo + 'a';
+    return text;
+}
+console.log(appendA(typo));
+*/
