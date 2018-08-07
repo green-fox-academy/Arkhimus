cc51e6e21812e85bde6061c318d1f1af433795f6
Adam Reznek <reznek.adam@gmail.com>
2018-08-07 11:37:33 +0200

    add(file): solar-system (not the best)

--- week-02/day-2/simple-replace.ts
+++ week-02/day-2/simple-replace.ts
@@ -1,4 +1,4 @@
-export{}
+export { }
 'use strict';
 
 let example: string = 'In a dishwasher far far away';
@@ -8,6 +8,11 @@ let example: string = 'In a dishwasher far far away';
 // Expected ouput: In a galaxy far far away
 console.log(example);
 
+let replacedExample: string = example.replace('dishwasher', 'galaxy');
+console.log(replacedExample);
+
+/*
 let replace = /dishwasher/;
 let newExample = example.replace(replace, 'galaxy');
 console.log(newExample);
+*/
---
+++ week-02/day-2/solar-system.ts
@@ -0,0 +1,27 @@
+export{}
+
+'use strict';
+// Saturn is missing from the planetList
+// Insert it into the correct position
+// Create a function called putSaturn() which has list parameter and returns the correct list.
+// bonus for using some built in methods
+
+let planetList: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Uranus', 'Neptune'];
+
+
+
+// Expected output: "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Uranus", "Neptune", "Saturn"
+
+function putSaturn(planet: string []){
+    planet ['']= planetList.push("Satrun");
+    return planet;
+}
+
+console.log(putSaturn(planetList));
+
+
+
+/* console.log(putSaturn(planetList));
+
+export = putSaturn;
+*/
