1b81bb2da7e92dfd40d4ad04cc0292c39d0b1e5a
Adam Reznek <reznek.adam@gmail.com>
2018-08-07 13:13:30 +0200

    mod(file): student-counter.ts 100%

--- week-02/day-2/student-counter.ts
+++ week-02/day-2/student-counter.ts
@@ -1,14 +1,14 @@
-export{}
+export { }
 
 'use strict';
 
 const students: any[] = [
-  {name: 'Theodor', age: 3, candies: 2},
-  {name: 'Paul', age: 9.5, candies: 2},
-  {name: 'Mark', age: 12, candies: 5},
-  {name: 'Peter', age: 7, candies: 3},
-  {name: 'Olaf', age: 12, candies: 7},
-  {name: 'George', age: 10, candies: 1}
+    { name: 'Theodor', age: 3, candies: 2 },
+    { name: 'Paul', age: 9.5, candies: 2 },
+    { name: 'Mark', age: 12, candies: 5 },
+    { name: 'Peter', age: 7, candies: 3 },
+    { name: 'Olaf', age: 12, candies: 7 },
+    { name: 'George', age: 10, candies: 1 }
 ];
 
 // create a function that takes a list of students and logs: 
@@ -16,7 +16,7 @@ const students: any[] = [
 // create a function that takes a list of students and logs:
 // - Sum of the age of people who have lass than 5 candies
 
-function candieSum(candie: any ){
+function candieSum(candie: any []) {
     let candieNumber: number = 0;
     candie.forEach(elem => {
         candieNumber += elem.candies;
@@ -24,3 +24,37 @@ function candieSum(candie: any ){
     console.log(candieNumber);
 };
 candieSum(students);
+/*
+function sumCandies (list: any[]){
+    let temp: number = 0;
+    list.forEach ((elem) => {
+        temp += elem['candies'];
+    });
+    return temp;
+}
+console.log(sumCandies(students));
+*/
+
+/*
+function ageSum(listed: any[]) {
+    let ageTemp: number = 0;
+    listed.forEach((element) => {
+        if (element['candies'] < 5) {
+            ageTemp += element['age'];
+        }
+        return ageTemp;
+    });
+}
+console.log(ageSum(students));
+*/
+
+function ageSum(candie: any []) {
+    let ageTemp: number = 0;
+    candie.forEach(elem => {
+        if (elem.candies < 5) {
+            ageTemp += elem.age;
+        }
+    });
+    console.log(ageTemp);
+};
+ageSum(students);
