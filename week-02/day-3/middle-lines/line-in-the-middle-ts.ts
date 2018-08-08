11df78dcf38095d07c6997ec6711998b4ea552a8
Adam Reznek <reznek.adam@gmail.com>
2018-08-08 10:43:44 +0200

    add(file): line in the middle

---
+++ week-02/day-3/middle-lines/line-in-the-middle-ts.ts
@@ -0,0 +1,23 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+//canvas.width = canvas.scrollWidth;
+//canvas.height = canvas.scrollHeight;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// draw a red horizontal line to the canvas' middle.
+// draw a green vertical line to the canvas' middle.
+
+ctx.beginPath();
+ctx.strokeStyle= 'red';
+ctx.moveTo(250,200);
+ctx.lineTo(350,200);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle= 'green';
+ctx.moveTo(300,150);
+ctx.lineTo(300,250);
+ctx.stroke();
