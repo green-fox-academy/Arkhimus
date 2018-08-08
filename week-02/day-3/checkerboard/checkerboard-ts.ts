1e6c4578c6a40d3e7f2eb0419471b3a435120459
Adam Reznek <reznek.adam@gmail.com>
2018-08-08 16:12:37 +0200

    add(file): checkerboard

---
+++ week-02/day-3/checkerboard/checkerboard-ts.ts
@@ -0,0 +1,25 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Fill the canvas with a checkerboard pattern.
+
+let x: number = 0;
+let y: number = 0;
+let size: number = 20; //kb. 30-as loop kell
+
+for (let i: number = 0; i < 20; i++) {
+    ctx.fillRect(x, y, size, size);
+    for (let j: number = 0; j < 30; j++) {
+        ctx.fillRect(x, y, size, size);
+        x = x + 40;
+    }
+    if (i % 2 == 0) {
+        x = 20;
+    } else {
+        x = 0;
+    }
+    y = y + 20;
+}
