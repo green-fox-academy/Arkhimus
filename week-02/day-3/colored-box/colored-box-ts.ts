62972d4e695f2658af9b65363660becae85f4a13
Adam Reznek <reznek.adam@gmail.com>
2018-08-08 10:51:58 +0200

    add(file): colored box

---
+++ week-02/day-3/colored-box/colored-box-ts.ts
@@ -0,0 +1,31 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// draw a box that has different colored lines on each edge.
+
+ctx.beginPath();
+ctx.moveTo(250,150);
+ctx.strokeStyle='green';
+ctx.lineTo(250,250);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(250,250);
+ctx.strokeStyle='red';
+ctx.lineTo(350,250);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(350,250);
+ctx.strokeStyle='orange';
+ctx.lineTo(350,150);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(350,150);
+ctx.strokeStyle='blue';
+ctx.lineTo(250,150);
+ctx.stroke();
