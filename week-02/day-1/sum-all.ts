83c95a94f2011f3f7b4a19b6cc018f2dd2342865
Adam Reznek <reznek.adam@gmail.com>
2018-08-09 16:39:57 +0200

    add(file): sum all

---
+++ week-02/day-1/sum-all.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
+
+let ai: number [] = [3, 4, 5, 6, 7];
+let temp: number = 0;
+
+ai.forEach(element => {
+    temp = temp + element;
+});
+console.log(temp);
