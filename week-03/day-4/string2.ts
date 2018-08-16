109bba7db7bcac7449da55293bba05dc272daf40
Adam Reznek <reznek.adam@gmail.com>
2018-08-16 15:03:50 +0200

    add(file) string2.ts

---
+++ week-03/day-4/string2.ts
@@ -0,0 +1,14 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+'use strict'
+
+'xxxyyyxyxyxxxxyyyyyxyxyxyyy'
+
+function removeX(remove: string): string{
+  if (remove.indexOf('x')=== -1){
+    return remove;
+  } else{
+    return remove = removeX(remove.replace('x',''));
+  }
+}
+console.log(removeX('xxxyyyxyxyxxxxyyyyyxyxyxyyy'));
