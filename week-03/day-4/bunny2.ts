436234df5e01ad9b2c4797b211061ca3acca5139
Adam Reznek <reznek.adam@gmail.com>
2018-08-16 12:56:38 +0200

    add(files): bunnies EVERYWHEREEEE

---
+++ week-03/day-4/bunny2.ts
@@ -0,0 +1,16 @@
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+'use strict'
+
+function bunnyEarsZwei(bunnies: number): number {
+  if (bunnies % 2 === 1) {
+    return 0;
+  } else {
+    return 3 + bunnyEarsZwei(bunnies - 1);
+  }
+}
+
+console.log(bunnyEarsZwei(10));
