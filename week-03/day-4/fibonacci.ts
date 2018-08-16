d8b2048750033776b1e1dfe61fca3631bdd40856
Adam Reznek <reznek.adam@gmail.com>
2018-08-16 16:48:28 +0200

    add(file): refactorio.ts

--- week-03/day-4/fibonacci.ts
+++ week-03/day-4/fibonacci.ts
@@ -8,10 +8,8 @@
 'use strict'
 
 function fibonacci(n: number): number{
-  if (n === 0){
-    return 0;
-  } else if(n === 1){
-    return 1;
+  if (n === 0 || n === 1){
+    return n;
   } else {
     return fibonacci(n-2) + fibonacci(n-1);
   }
---
+++ week-03/day-4/refactorio.ts
@@ -0,0 +1,11 @@
+// Create a recursive function called `refactorio`
+// that returns it's input's factorial
+
+function refactorio(n: number): number {
+  if (n === 1) {
+    return 1;
+  } else {
+    return n = n * refactorio(n - 1);
+  }
+}
+console.log(refactorio(10));
