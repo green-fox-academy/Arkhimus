f37233315736d38247fd7b7a35dcea46d00c266d
Adam Reznek <reznek.adam@gmail.com>
2018-08-16 10:48:52 +0200

    add(file): power.ts

--- week-03/day-4/power.ts
+++ week-03/day-4/power.ts
@@ -1,2 +1,10 @@
 'use strict'
 
+function powerN(base: number, exponent: number): number {
+  if (exponent == 1) {
+    return base;
+  } else {
+    return base * powerN(base, exponent - 1);
+  }
+}
+console.log(powerN(522, 74));
