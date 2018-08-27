ad92de113577c43ed520efd37d22ac8fa39a0e16
Adam Reznek <reznek.adam@gmail.com>
2018-08-27 16:19:10 +0200

    mod(test): false with a null input

--- week-05/day-1/anagram-tdd/anagram-test.ts
+++ week-05/day-1/anagram-tdd/anagram-test.ts
@@ -8,7 +8,12 @@ test('is an anagram bro/brotherina?', test => {
   test.end();
 });
 
-test('is an anagram bro/brotherina?', test => {
+test('is an anagram with lower/upper case bro/brotherina?', test => {
   test.equal(anagramCompare('Listen', 'Silent'), true);
+  test.end();
+});
+
+test('is the input null bro/brotherina?', test => {
+  test.equal(anagramCompare('Listen', null), false);
   test.end();
 });
--- week-05/day-1/anagram-tdd/anagram.ts
+++ week-05/day-1/anagram-tdd/anagram.ts
@@ -1,6 +1,6 @@
 'use strict';
 let anagramBase: string = 'Listen';
-let anagramModded: string = 'Silent';
+let anagramModded: string = null;
 
 export function anagramCompare(quote1: string, quote2: string): boolean {
   return (quote1.toLowerCase().split('').sort().toString() === quote2.toLocaleLowerCase().split('').sort().toString());
