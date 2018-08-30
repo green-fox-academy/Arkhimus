f90780624e00a48db8aeb388a660e82d060e65bc
Adam Reznek <reznek.adam@gmail.com>
2018-08-30 09:14:04 +0200

    mod(files):

--- week-04/day-2/aircraft/air-test.ts
+++ week-04/day-2/aircraft/air-test.ts
@@ -4,16 +4,10 @@ import { Aircraft } from "./aircraft";
 import { F16 } from "./f16";
 import { F35 } from "./f35";
 
-<<<<<<< HEAD
 'use strict'
 
 let plane1: Aircraft = new F16();
 let plane2: Aircraft = new F35();
-
-=======
-let plane1: Aircraft = new F16();
-let plane2: F35 = new F35();
->>>>>>> 80d72af822d15b10cc977c95b1d7e656a760d2c7
 
 console.log(plane1, plane2);
 plane1.refill(5);
--- week-05/day-1/number-converter/number-converter.ts
+++ week-05/day-1/number-converter/number-converter.ts
@@ -1,2 +1,15 @@
 'use strict';
 
+let one: string = '1';
+let thousand: string = '000';
+
+let numberOne: number = 1;
+let numberThousand: number = 1000;
+
+function numbersToString(inputInteger: number): any {
+  if (inputInteger.toString().split('').length < 3){
+    return inputInteger.toString().join()
+  }
+}
+
+numbersToString(12134);
