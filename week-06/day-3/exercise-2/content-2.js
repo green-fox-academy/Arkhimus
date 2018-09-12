9c8503f2d3dda78a791821beffed8869fc9cfc26
Adam Reznek <reznek.adam@gmail.com>
2018-09-12 14:21:36 +0200

    mod(exercise 2): background limegreen

--- week-06/day-3/exercise-2/content-2.js
+++ week-06/day-3/exercise-2/content-2.js
@@ -1,8 +1,12 @@
 'use strict';
 
+
 let fillWithContent = document.getElementsByTagName('li');
 let content = ['apple', 'banana', 'cat', 'dog'];
 
 for (let index = 0; index < fillWithContent.length; index++) {
   fillWithContent[index].innerText = content[index];
 }
+
+let getGreen = document.getElementsByTagName('ul');
+getGreen[0].setAttribute("style", "background-color: limegreen");
