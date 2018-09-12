9cee835d1499c271fd5c4abff67eb59ba9979376
Adam Reznek <reznek.adam@gmail.com>
2018-09-12 17:04:35 +0200

    add(exercise 5): add heading and new asteroids and everything

--- week-06/day-3/exercise-5/createelement-1.js
+++ week-06/day-3/exercise-5/createelement-1.js
@@ -1,2 +1,22 @@
 'use strict';
 
+let asteroidList = document.querySelector('ul.asteroids');
+
+let newAsteroidGreenFox = document.createElement('li');
+newAsteroidGreenFox.id = 'b555';
+newAsteroidGreenFox.textContent = 'The Green Fox';
+
+let newAsteroidLamp = document.createElement('li');
+newAsteroidLamp.id = 'b567';
+newAsteroidLamp.textContent = 'The Lamplighter';
+
+asteroidList.appendChild(newAsteroidGreenFox);
+asteroidList.appendChild(newAsteroidLamp);
+
+let heading = document.querySelector('div.container');
+
+let newHeading = document.createElement('h1');
+newHeading.id = 'heading1orWhatever';
+newHeading.textContent = 'I can add elements to the DOM!';
+
+heading.appendChild(newHeading);
