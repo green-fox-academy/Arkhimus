1987cf9e837e30dd55d093ccca44a893fa76e52b
Adam Reznek <reznek.adam@gmail.com>
2018-09-12 14:52:01 +0200

    mod(exercise 3): link to greenfox

--- week-06/day-3/exercise-3/attribute-1.js
+++ week-06/day-3/exercise-3/attribute-1.js
@@ -4,3 +4,6 @@ let imgUrl = document.querySelector('img');
 console.log(imgUrl.src);
 imgUrl.src = "https://study.com/cimages/multimages/16/gray_wolf_5584759091_wikimedia.jpg";
 
+let newLink = document.querySelector('a');
+newLink.href = "https://www.greenfoxacademy.com/";
+console.log(newLink.href);
