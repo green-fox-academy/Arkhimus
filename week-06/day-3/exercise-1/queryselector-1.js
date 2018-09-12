6a73d321c9b0ddf933afcc287ef48f49d93bda0f
Adam Reznek <reznek.adam@gmail.com>
2018-09-12 12:18:30 +0200

    mod(DOM): first exercise

--- week-06/day-3/exercise-1/queryselector-1.js
+++ week-06/day-3/exercise-1/queryselector-1.js
@@ -4,10 +4,10 @@ let king = document.getElementById('b325');
 console.log(king);
 
 let businessLamp = document.getElementsByClassName('big');
-console.log(businessLamp[0],businessLamp[1]);
+console.log(businessLamp[0], businessLamp[1]);
 
 let conceitedKing = document.querySelectorAll('.container .asteroid');
-console.log(conceitedKing[0],conceitedKing[1]);
+console.log(conceitedKing[0], conceitedKing[1]);
 
-let noBusiness = conceitedKing + ' ' + document.getElementsByClassName('b329');
-console.log(noBusiness);
+let noBusiness = document.getElementsByTagName('div');
+console.log(noBusiness[0], noBusiness[1], noBusiness[2]);
