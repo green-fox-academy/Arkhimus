d9c164e1dcabfaab4418188b07f9a0e950fe5651
Adam Reznek <reznek.adam@gmail.com>
2018-09-17 16:42:12 +0200

    add(): reverser app is up and running

---
+++ week-07/day-1/reverser/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const jsonParser = bodyParser.json();
+const PORT = 4000;
+
+app.use(jsonParser);
+
+app.get('/', (req,res) => {
+  res.send('All right baby!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is uuuup dude at port: ${PORT}`);
+});
