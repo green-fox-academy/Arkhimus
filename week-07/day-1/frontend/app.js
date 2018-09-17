7e7580e2b3e359f0494b40188bdb5952368de071
Adam Reznek <reznek.adam@gmail.com>
2018-09-17 12:07:23 +0200

    mod(): app.js path require works

--- week-07/day-1/frontend/app.js
+++ week-07/day-1/frontend/app.js
@@ -3,9 +3,10 @@
 const express = require('express');
 const app = express();
 const PORT = 3030;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
