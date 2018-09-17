2ea7b4d14dbc3947a374bd84a165ab5850658aec
Adam Reznek <reznek.adam@gmail.com>
2018-09-17 14:15:24 +0200

    mod(): doubling and greet function fully operational

--- week-07/day-1/frontend/app.js
+++ week-07/day-1/frontend/app.js
@@ -1,12 +1,48 @@
 'use strict';
 
 const express = require('express');
-const app = express();
-const PORT = 3030;
 const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling', (req, res) => {
+  if (req.query.input !== undefined) {
+    res.json({
+      "received": req.query.input,
+      "result": req.query.input * 2,
+    });
+  } else {
+    res.json({
+      "error": "Please provide an input!"
+    });
+  }
+});
+
+app.get('/greeter', (req, res) => {
+  if (req.query.name && req.query.title) {
+    res.json({
+      "welcome_message": `Oh, hi there ${req.query.name}, my dear ${req.query.title}!`,
+    });
+  } else if (!req.query.name) {
+    res.json({
+      "error": "Please provide a name!",
+    });
+  } else if (!req.query.title) {
+    res.json({
+      "error": "Please provide a title!",
+    });
+  }
+});
+
+app.get('/appenda/{appendable}', (req,res) => {
+
 });
 
 app.listen(PORT, () => {
