8c8db4171ceb0a0ac70dd56f4ffcbdc5dfadc196
Adam Reznek <reznek.adam@gmail.com>
2018-09-18 15:22:04 +0200

    add(): remove function (in progress)

--- week-07/day-2/callback-1.js
+++ week-07/day-2/callback-1.js
@@ -5,9 +5,12 @@ const mapWith = (array, callback) => {
   array.map((element) => {
     output.push(callback(element));
   });
+
   // The mapWith() function should iterate over the given array and call the callback function on every element.
   // It stores the callback return values in the output.
   // The mapWith() should return with the output array.
+
+
 
   return output;
 }
@@ -21,11 +24,22 @@ const addOne = (number) => {
 console.log(mapWith([1, 2, 3], addOne));
 //expected result: [2, 3, 4]
 
-// // Exercise 2:
+// Exercise 2:
 
-// // Create a callback function which removes every second character from a string
+const removeSecondLetter = (string) => {
+  for (let i = 0; i < string.length; i++) {
+    for (let j = 0; j < string[i].length; j++) {
+      if (j % 2 === 0) {
+        string[i][j] = '';
+      }
+    }
+  }
+  return string;
+}
 
-// const words = ['map', 'reduce', 'filter'];
+// Create a callback function which removes every second character from a string
 
-// console.log(mapWith(words, removeSecondLetter));
-// // expected result: ['mp','rdc', 'fle']
+let words = ['map', 'reduce', 'filter'];
+
+console.log(mapWith(words, removeSecondLetter));
+// expected result: ['mp','rdc', 'fle']
