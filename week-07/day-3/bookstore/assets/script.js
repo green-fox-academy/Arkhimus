d7a3095e9762c9821e79d214bd916a519ef20057
Adam Reznek <reznek.adam@gmail.com>
2018-09-19 17:22:45 +0200

    mod(): books listed in table with appropriate properties

--- week-07/day-3/bookstore/assets/script.js
+++ week-07/day-3/bookstore/assets/script.js
@@ -22,26 +22,33 @@ window.onload = () => {
   http.onload = () => {
     if (http.status === 200) {
       const booksName = JSON.parse(http.response);
-      console.log(booksName);
       booksName.forEach(book => {
-        let newTitle = document.createElement('p');
-        containerDiv.appendChild(newTitle);
-        newTitle.innerText = book.book_name;
-        newTitle.classList.add('bookItem');
-        // newTitle.setAttribute('src', book.url)
 
         let newRow = document.createElement('tr');
 
-        let newEntry = document.createElement('td');
-        newEntry.innerText = book.book_name;
-        newRow.appendChild(newEntry);
-        
+        let newBookTitle = document.createElement('td');
+        newBookTitle.innerText = book.book_name;
+        newRow.appendChild(newBookTitle);
+
+        let newBookAuthor = document.createElement('td');
+        newBookAuthor.innerText = book.aut_name;
+        newRow.appendChild(newBookAuthor);
+
+        let newCategory = document.createElement('td');
+        newCategory.innerText = book.cate_descrip;
+        newRow.appendChild(newCategory);
+
+        let newPublisher = document.createElement('td');
+        newPublisher.innerText = book.pub_name;
+        newRow.appendChild(newPublisher);
+
+        let newPrice = document.createElement('td');
+        newPrice.innerText = book.book_price;
+        newRow.appendChild(newPrice);
+
         let table = document.querySelector('#tableId');
         table.appendChild(newRow);
       });
-      // let table = documnet.createElement('tr');
-      // table.classList.add('table');
-
     }
   }
   http.send();
