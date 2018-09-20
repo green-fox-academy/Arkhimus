ad7aa9495c157d25f9316ed7104a686c4de74c14
Adam Reznek <reznek.adam@gmail.com>
2018-09-20 16:37:47 +0200

    mod(): upvote downvote in one endpoint

--- week-07/day-4/reddit/server.js
+++ week-07/day-4/reddit/server.js
@@ -40,33 +40,52 @@ app.post('/api/posts', jsonParser, (req, res) => {
   }
 });
 
-app.put(`/posts/:id/upvote`, jsonParser, (req, res) => {
-  if (req.body.title && req.body.url && res.statusCode === 200) {
-    conn.query(`UPDATE posts SET score = score + 1 WHERE id=${req.params.id};`, (err, result) => {
-      if (err) {
-        console.log('failure', err.message);
-        return;
-      }
-      conn.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, voteResult) => {
-        res.status(200).json(voteResult);
-      });
-    });
+// app.put(`/posts/:id/upvote`, jsonParser, (req, res) => {
+//   if (req.body.title && req.body.url && res.statusCode === 200) {
+//     conn.query(`UPDATE posts SET score = score + 1 WHERE id=${req.params.id};`, (err, result) => {
+//       if (err) {
+//         console.log('failure', err.message);
+//         return;
+//       }
+//       conn.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, voteResult) => {
+//         res.status(200).json(voteResult);
+//       });
+//     });
+//   }
+// });
+
+// app.put(`/posts/:id/downvote`, jsonParser, (req, res) => {
+//   if (req.body.title && req.body.url && res.statusCode === 200) {
+//     conn.query(`UPDATE posts SET score = score - 1 WHERE id=${req.params.id};`, (err, result) => {
+//       if (err) {
+//         console.log('failure', err.message);
+//         return;
+//       }
+//       conn.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, voteResult) => {
+//         res.status(200).json(voteResult);
+//       });
+//     });
+//   }
+// });
+
+app.put(`/posts/:id/:action`, jsonParser, (req, res) => {
+  let addVote = '';
+  if (req.params.action === 'upvote') {
+    addVote = '+';
+  } else if (req.params.action === 'downvote') {
+    addVote = '-';
   }
+  conn.query(`UPDATE posts SET score = score ${addVote} 1 WHERE id=${req.params.id};`, (err, result) => {
+    if (err) {
+      console.log('failure', err.message);
+      return;
+    }
+    conn.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, voteResult) => {
+      res.status(200).json(voteResult);
+    });
+  });
 });
 
-app.put(`/posts/:id/downvote`, jsonParser, (req, res) => {
-  if (req.body.title && req.body.url && res.statusCode === 200) {
-    conn.query(`UPDATE posts SET score = score - 1 WHERE id=${req.params.id};`, (err, result) => {
-      if (err) {
-        console.log('failure', err.message);
-        return;
-      }
-      conn.query(`SELECT * FROM posts WHERE id=${req.params.id}`, (error, voteResult) => {
-        res.status(200).json(voteResult);
-      });
-    });
-  }
-});
 
 app.get('/api/posts', (req, res) => {
   conn.query(`SELECT * FROM posts`, (err, result) => {
