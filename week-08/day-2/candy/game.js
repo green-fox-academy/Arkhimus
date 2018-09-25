05e11b79a8170d978dd704d4fcf5bd6bfb1768b7
Adam Reznek <reznek.adam@gmail.com>
2018-09-25 17:04:00 +0200

    mod(): candy generator and ratio log

--- week-08/day-2/candy/game.js
+++ week-08/day-2/candy/game.js
@@ -1,12 +1,48 @@
 'use strict';
 
 const candies = document.querySelector('.candies');
+const lollypops = document.querySelector('.lollypops')
 const createCandies = document.querySelector('.create-candies');
+const buy = document.querySelector('.buy-lollypops');
+const lollybutton = document.querySelector('.buy-lollypops');
+const speed = document.querySelector('.speed');
 
-let currentCandies =  10000;
+let currentCandies = 10000;
+let currentLollypops = lollypops.innerText;
+let generateSpeed = 0;
+
 candies.innerText = currentCandies;
 
 createCandies.addEventListener('click', () => {
-  currentCandies++;
+  currentCandies += 10;
   candies.innerText = currentCandies;
+  if (currentCandies > 100) {
+    lollybutton.disabled = false;
+  }
 });
+
+buy.addEventListener('click', () => {
+  currentCandies -= 100;
+  candies.innerText = currentCandies;
+  lollypops.innerText = `${currentLollypops}🍭`;
+  currentLollypops = lollypops.innerText;
+  if (currentCandies < 100) {
+    lollybutton.disabled = true;
+  }
+});
+
+const candiesForLollypop = () => {
+  if (currentLollypops.length / 2 >= 10) {
+    currentCandies = currentCandies + generateSpeed;
+    candyCreation();
+  }
+}
+
+const candyCreation = () => {
+  if (currentLollypops.length / 2 >= 10) {
+    generateSpeed = Math.floor((currentLollypops.length / 2) / 10);
+    speed.innerText = generateSpeed;
+  }
+}
+
+setInterval(candiesForLollypop, 1000);
