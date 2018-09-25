baa9c63624dc40819e6b693464b19f54644509de
Adam Reznek <reznek.adam@gmail.com>
2018-09-25 16:03:13 +0200

    add(): three clicks

---
+++ week-08/day-2/three-clicks/clickthree.js
@@ -0,0 +1,20 @@
+'use strict';
+
+window.onload = () => {
+
+  const button = document.querySelector('button');
+  const h1 = document.querySelector('h1');
+
+  let counter = 0;
+
+  setTimeout(() => {
+    button.addEventListener('click', () => {
+      counter++;
+      if (counter === 3) {
+        h1.innerHTML = '5 seconds elapsed and clicked 3 times';
+      }
+    });
+  }, 5000);
+}
+
+
