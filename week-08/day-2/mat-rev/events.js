a253bd726bb3c92a347b84838ee161128bce5138
Adam Reznek <reznek.adam@gmail.com>
2018-09-25 10:41:38 +0200

    mod(): click hover random color

--- week-08/day-2/mat-rev/events.js
+++ week-08/day-2/mat-rev/events.js
@@ -1,4 +1,20 @@
 'use strict';
 
 const click = document.querySelector('.click');
+const hover = document.querySelector('.hover');
 
+const getRandom = () => {
+  return Math.floor(Math.random() * 255);
+};
+
+const getRandomColor = () => {
+  return `rgb(${getRandom()},${getRandom()},${getRandom()})`;
+};
+
+click.addEventListener('click', (event) => {
+  click.style.backgroundColor = getRandomColor();
+});
+
+hover.addEventListener('mouseenter', (event) => {
+  hover.style.backgroundColor = getRandomColor();
+});
