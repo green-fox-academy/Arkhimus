94badd097dcbc5e00aa63894e6e0e01d86409183
Adam Reznek <reznek.adam@gmail.com>
2018-09-27 10:09:49 +0200

    mod(): server setup

--- week-08/day-4/mat-rev/server.js
+++ week-08/day-4/mat-rev/server.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const PORT = 3030;
+
+app.listen(PORT, () => {
+  console.log(`Server runz at: ${PORT}`);
+});
