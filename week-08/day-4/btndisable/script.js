aa1ff8a2835805fb4f775520bcd4a58658348d90
Adam Reznek <reznek.adam@gmail.com>
2018-09-27 15:42:16 +0200

    mod(): adding logic to disabled buttons (not working yet)

--- week-08/day-4/btndisable/script.js
+++ week-08/day-4/btndisable/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
-const subscribe = document.querySelector('subscribe');
+const petform = document.querySelector('subscribe');
 
+let radios = document.forms["choosepet"].elements["pet"];
+  for(radio in radios) {
+    radio.onclick = function() {
+        return radio.value;
+    }
+  if (radio.value === 'dog' || radio.value === 'cat') {
+    petform.disabled = false;
+  }
+}
