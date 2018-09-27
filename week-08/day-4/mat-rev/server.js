ab144f389b528feacbe7c49d778c015c560a8681
Adam Reznek <reznek.adam@gmail.com>
2018-09-27 10:14:39 +0200

    mod(): post endpoint

--- week-08/day-4/mat-rev/server.js
+++ week-08/day-4/mat-rev/server.js
@@ -6,6 +6,14 @@ const cors = require('cors');
 const app = express();
 const PORT = 3030;
 
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/postElements', (req,res) => {
+  console.log(req.body);
+  res.json(req.body);
+});
+
 app.listen(PORT, () => {
   console.log(`Server runz at: ${PORT}`);
 });
