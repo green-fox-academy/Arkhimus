97f499498349fab32078544d2cd9a6637338a519
Adam Reznek <reznek.adam@gmail.com>
2018-09-27 16:42:21 +0200

    feat(): sign up activate if pet selected

--- week-08/day-4/btndisable/script.js
+++ week-08/day-4/btndisable/script.js
@@ -4,5 +4,22 @@ window.onload = () => {
   const subscribe = document.querySelector('.subscribe');
   const signup = document.querySelector('.signupbtn');
 
-  const
+  const dog = document.querySelector('.dog');
+  const cat = document.querySelector('.cat');
+  const fish = document.querySelector('.fish');
+  const yes = document.querySelector('.yes');
+  const no = document.querySelector('.no');
+
+  dog.addEventListener('change', () => {
+    signup.disabled = false;
+  });
+
+  cat.addEventListener('change', () => {
+    signup.disabled = false;
+  });
+
+  fish.addEventListener('change', () => {
+    signup.disabled = false;
+  });
+
 }
