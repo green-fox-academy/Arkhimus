fbcb297be6a8d0c9b8f2bf6c3afb5e2a64a7d507
Adam Reznek <reznek.adam@gmail.com>
2018-09-27 17:26:52 +0200

    mod(): classes added to selectors and movies

---
+++ week-08/day-4/movie/script.js
@@ -0,0 +1,11 @@
+window.onload = () => {
+  const scifi = document.querySelector('.scifiM');
+  const drama = document.querySelector('.dramaM');
+  const comedy = document.querySelector('.comedyM');
+
+  const scifiSelected = document.querySelector('.scifi');
+  const dramaSelected = document.querySelector('.drama');
+  const comedySelected = document.querySelector('.comedy');
+
+
+}
