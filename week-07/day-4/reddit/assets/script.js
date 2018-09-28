27c86949de896df51655d1e0639e3104b9e077c4
Adam Reznek <reznek.adam@gmail.com>
2018-09-28 11:50:19 +0200

    feat(): all posts listed in separated divs

--- week-07/day-4/reddit/assets/script.js
+++ week-07/day-4/reddit/assets/script.js
@@ -10,14 +10,25 @@ window.onload = () => {
   .then(res => {
     res.forEach(element => {
       allPosts.push(element);
+      let postDiv = document.createElement("div");
+      let title = document.createElement("h3");
+      let postUrl = document.createElement("a");
+      let date = document.createElement("p");
+      let score = document.createElement("p");
+      let user = document.createElement("p");
+      postDiv.classList.add('post');
+      postList.appendChild(postDiv);
+      postDiv.appendChild(title).innerText = element.title;
+      postDiv.appendChild(postUrl).innerText = element.url;
+      postDiv.appendChild(date).innerText = element.timestamp;
+      postDiv.appendChild(score).innerText = element.score;
+      postDiv.appendChild(user).innerText = element.user;
+
       
+
     });
   });
   
   
-
-
-
-
 
 }
